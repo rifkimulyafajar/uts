88ab73775329f3ecf6c89a230c27027de8977c71
rifkimulyafajar <rifkifajar25@gmail.com>
2020-04-05 20:27:54 +0700

    new project, Splashscrenn

---
+++ app/src/main/java/com/example/uts/LapFutsalActivity.java
@@ -0,0 +1,14 @@
+package com.example.uts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LapFutsalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lap_futsal);
+    }
+}
